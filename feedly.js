3dbc69e79b6504f6199b9021190c643bf4b0461f
Artur Paikin <artur@arturpaikin.com>
2018-04-25 16:11:28 -0400

    Update feedly.js

--- feedly.js
+++ feedly.js
@@ -1,5 +1,5 @@
 javascript:(function(){
-  var rssURL = document.querySelector('[type="application/rss+xml"]').getAttribute('href');
+  var rssURL = document.querySelector('[type="application/rss+xml"], [type="application/atom+xml"]').getAttribute('href');
   var protocol = rssURL.match(/^([a-z0-9]+):\/\//);
   if (!protocol) rssURL = new URL(rssURL, window.location.protocol + '//' + window.location.hostname);
   window.open('http://feedly.com/i/subscription/feed/' + rssURL, '_blank');
